3966250dd25095a674496491287acefd6cd3d446
irfan-fauzi <irfanwintech@yahoo.com>
2023-06-01 10:42:38 +0700

    create post controller

--- controllers/post.js
+++ controllers/post.js
@@ -1,10 +1,28 @@
-import Post from "../models/Post";
+/* eslint-disable linebreak-style */
+/* eslint-disable import/prefer-default-export */
 
-// Read
-export const getFeedPosts = async (req, res) => {
+import Post from '../models/Post';
+import User from '../models/User';
+
+export const createPost = async (req, res) => {
   try {
-    
-  } catch (error) {
-    
+    const { userId, description, picturePath } = req.body;
+    const user = await User.findById(userId);
+    const newPost = new Post({
+      userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      location: user.location,
+      description,
+      userPicturePath: user.picturePath,
+      picturePath,
+      likes: {},
+      comments: [],
+    });
+    await newPost.save();
+    const post = await Post.find();
+    res.status(201).json(post);
+  } catch (err) {
+    res.status(409).json({ message: err.message });
   }
-}
+};
